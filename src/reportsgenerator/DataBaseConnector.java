6f41feba16c365a2357051e79f38da02bc6f2990
Peta <peta@MacBook-Pro-de-Peta.local>
2024-03-21 20:06:14 +0000

    amending my DataBaseConnector to be able to display three columns from the students table

--- src/reportsgenerator/DataBaseConnector.java
+++ src/reportsgenerator/DataBaseConnector.java
@@ -15,23 +15,34 @@ import java.sql.ResultSet;
  * @author peta
  */
 public class DataBaseConnector {
-    private final String DB_URL = "jdbc:mysql://localhost";
+    private final String DB_URL = "jdbc:mysql://localhost:3306/Reports";
     private final String USER = "integrated_CA";
     private final String PASSWORD = "integrated_CA";
     
     public void readDataBase() throws SQLException {
-    try {
-        Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM students;");
-        while (resultSet.next()) {
-            System.out.println("Hello");
+        try {
+            String sql = "SELECT * FROM Reports.students";
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        connection.close();
-    } catch (Exception e) {
-        e.printStackTrace();
     }
-}
-}
+    }
+
+
+
+
+
+
+
+
+
 
 
