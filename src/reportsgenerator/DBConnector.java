1200d3bcb9de6c9d8ffbc480d3a779a3ca26d5e2
Peta <peta@MacBook-Pro-de-Peta.local>
2024-03-21 18:13:34 +0000

    created a new class for my database connector

---
+++ src/reportsgenerator/DBConnector.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reportsgenerator;
+
+/**
+ *
+ * @author peta
+ */
+public class DBConnector {
+    private final String DB_URL = "jdbc:mysql://localhost";
+    private final String USER = "integrated_CA";
+    private final String PASSWORD = "integrated_CA";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
